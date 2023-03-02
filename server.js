fe04b432d2daeec71b805f92967b97ca2aeff2ca
shayanfpg9 <shfqaen@gmail.com>
2023-03-02 20:01:22 +0330

    change connection method

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+const { default: mongoose } = require("mongoose");
+
 require("dotenv").config();
 const express = require("express"),
   app = express(),
   morgan = require("morgan"),
-  AtomsRouter = require("./routes/Atoms.route");
+  AtomsRouter = require("./routes/Atoms.route"),
+  connect = require("./functions/connect");
 
-//listen:
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
-});
+//listen + connect to DB:
+connect(process.env.MONGOURI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 //middlewares:
 app.use(morgan("dev"));
