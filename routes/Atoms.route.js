48a69691813a7a497ebee039caba154560a55b32
shayanfpg9 <shfqaen@gmail.com>
2023-03-03 23:02:43 +0330

    remove export routers

--- routes/Atoms.route.js
+++ routes/Atoms.route.js
@@ -2,27 +2,6 @@ const express = require("express"),
   router = express.Router(),
   Atoms = require("../model/Atoms.model");
 
-//--------------------------------------------------------> Export
-
-//router / export -- All
-router.get("/export", (req, res) => {
-  res.json({
-    lang: "router",
-    method: "GET",
-    action: "Export all of the Atoms",
-  });
-});
-
-//router / export -- Single
-router.get("/export/:id", (req, res) => {
-  res.json({
-    lang: "router",
-    method: "GET",
-    action: "Export a single Atom",
-    params: req.params,
-  });
-});
-
 //--------------------------------------------------------> Get
 
 //router / Get -- All
