ceacee2778fb8dee2f3be6d2efa5c8e47a3ee3db
shayanfpg9 <shfqaen@gmail.com>
2023-03-03 23:10:30 +0330

    add controllers + fix error reporting

---
+++ controller/Atoms.controller.js
@@ -0,0 +1,40 @@
+const Atoms = require("../model/Atoms.model");
+
+const GetAll = async (req, res) => {
+  try {
+    const AllAtoms = await Atoms.find().sort({ number: 1 });
+
+    if (!AllAtoms) throw null;
+
+    res.json(AllAtoms);
+  } catch (e) {
+    res.statusCode(500).json({
+      method: "GET",
+      action: "Get all of the Atoms",
+      params: req.params,
+      error: e.errors || e,
+    });
+  }
+};
+
+const GetSingle = async (req, res) => {
+  try {
+    const Atom = await Atoms.find({
+      name: req.params.name[0].toUpperCase() + req.params.name.slice(1),
+    });
+
+    res.json(Atom);
+  } catch (e) {
+    res.statusCode(404).json({
+      method: "GET",
+      action: "Get a single Atom",
+      params: req.params,
+      error: e.errors || e,
+    });
+  }
+};
+
+module.exports = {
+  GetSingle,
+  GetAll,
+};
--- routes/Atoms.route.js
+++ routes/Atoms.route.js
@@ -1,29 +1,13 @@
+const { GetAll, GetSingle } = require("../controller/Atoms.controller");
 const express = require("express"),
-  router = express.Router(),
-  Atoms = require("../model/Atoms.model");
+  router = express.Router();
 
 //--------------------------------------------------------> Get
 
 //router / Get -- All
-router.get("/", async (req, res) => {
-  try {
-    const AllAtoms = await Atoms.find().sort({ number: 1 });
-
-    if (!AllAtoms) throw null;
-
-    res.json(AllAtoms);
-  } catch (e) {
-    console.error(e.errors || e);
-  }
-});
+router.get("/", GetAll);
 
 //router / GET -- Single
-router.get("/:name", async (req, res) => {
-  const Atom = await Atoms.find({
-    name: req.params.name[0].toUpperCase() + req.params.name.slice(1),
-  });
-
-  res.json(Atom);
-});
+router.get("/:name", GetSingle);
 
 module.exports = router;
