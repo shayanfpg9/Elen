9f5d151543c9579f6bd5ef437201bd40418028c2
shayanfpg9 <shfqaen@gmail.com>
2023-03-08 00:18:57 +0330

    import footer

--- client/src/component/Elen.jsx
+++ client/src/component/Elen.jsx
@@ -1,12 +1,14 @@
 import { Component } from "react";
 import "../styles/Main.scss";
+import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 
-export default class Elen extends Component{
+export default class Elen extends Component {
   render() {
     return (
       <>
         <Header></Header>
+        <Footer></Footer>
       </>
     );
   }
