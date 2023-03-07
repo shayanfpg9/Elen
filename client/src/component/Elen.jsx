13e3fdaa39e60df50f15f14634bb6a4dfd251b89
shayanfpg9 <shfqaen@gmail.com>
2023-03-07 23:57:07 +0330

    import header

--- client/src/component/Elen.jsx
+++ client/src/component/Elen.jsx
@@ -1,7 +1,13 @@
+import { Component } from "react";
 import "../styles/Main.scss";
+import Header from "./Header/Header";
 
-export default class Elen {
+export default class Elen extends Component{
   render() {
-    return <></>;
+    return (
+      <>
+        <Header></Header>
+      </>
+    );
   }
 }
