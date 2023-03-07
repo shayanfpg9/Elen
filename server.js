a04711664ba79b4b5927e3fc3cac45ec8f69b7d4
shayanfpg9 <shfqaen@gmail.com>
2023-03-07 19:13:02 +0330

    Error 404 handle

--- server.js
+++ server.js
@@ -22,3 +22,17 @@ connect(process.env.MONGOURI)
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/atom/", AtomsRouter);
+
+//404 error
+app.all("*", (req, res) => {
+  res.status(404).json({
+    method: req.method,
+    action: undefined,
+    path: req.path,
+    error: {
+      title: "Error 404",
+      msg: "Action is undefined",
+      status: 404,
+    },
+  });
+});
