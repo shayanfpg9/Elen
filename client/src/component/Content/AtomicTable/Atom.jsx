9f43db264576a5035e35b3cdfd8bd275b4111139
shayanfpg9 <shayanfpg9@duck.com>
2023-03-13 11:39:48 +0330

    Preparation for search + add attrs

--- client/src/component/Content/AtomicTable/Atom.jsx
+++ client/src/component/Content/AtomicTable/Atom.jsx
@@ -2,11 +2,17 @@ import styled, { css } from "styled-components";
 import { colors } from "./Categories";
 
 //style of AtomCles
-const AtomCel = styled.div`
+const AtomCel = styled.div.attrs((props) => ({
+  "data-category": props.category.join(" , "),
+  "data-phase": props.phase.toLowerCase(),
+}))`
   min-width: calc(var(--first-fibo) / 2);
   min-height: calc(var(--first-fibo) / 2);
   display: block;
-  background-color: ${(props) => colors[props.category[0]]};
+  background-color: ${(props) =>
+    props.category[0] === "unknown"
+      ? colors[props.category[1].replace("-", "")]
+      : colors[props.category[0]].replace("-", "")};
   border-radius: 0.3rem;
   margin: 0.1rem;
   ${(props) =>
@@ -34,10 +40,14 @@ const AtomCel = styled.div`
     font-size: 0.6rem;
     margin-left: 0.1rem;
   }
+
+  &.hide {
+    opacity: 0;
+  }
 `;
 
 export default function Atom(props) {
-  let PropCat = props.category;
+  let PropCat = props.category.replace("post-transition", "posttran");
   const category = [];
   [...Object.keys(colors), "metal"].forEach((cat) => {
     if (PropCat.includes(cat)) {
@@ -48,10 +58,12 @@ export default function Atom(props) {
 
   return (
     <AtomCel
+      data-name={props.name}
       number={props.number}
       position={props.position}
-      category={[...category]}
       phase={props.phase}
+      category={category}
+      className="Atom"
     >
       {props.symbol}
       <sup>{props.number}</sup>
