687d5871ba9837ff158e8e8ac9d5f51eb925dc24
shayanfpg9 <shayanfpg9@duck.com>
2023-03-14 19:25:03 +0330

    import info

--- client/src/component/Elen.jsx
+++ client/src/component/Elen.jsx
@@ -2,19 +2,24 @@ import { Component } from "react";
 import "../styles/Main.scss";
 import Table from "./Content/AtomicTable/Table";
 import Content from "./Content/Content";
+import Info from "./Content/Info/Info";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default class Elen extends Component {
   render() {
     return (
-      <>
+      <Router>
         <Header></Header>
         <Content>
-          <Table></Table>
+          <Routes>
+            <Route path="/table" element={<Table />} />
+            <Route path="/table/:atom" element={<Info />} />
+          </Routes>
         </Content>
         <Footer></Footer>
-      </>
+      </Router>
     );
   }
 }
