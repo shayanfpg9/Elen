ed6d699b38574e5965540559c695914c5f3eb0dd
shayanfpg9 <shayanfpg9@duck.com>
2023-03-15 12:40:33 +0330

    import loader + make loaded obj

--- client/src/component/Elen.jsx
+++ client/src/component/Elen.jsx
@@ -6,16 +6,40 @@ import Info from "./Content/Info/Info";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Loader from "./Loader/Loader";
 
 export default class Elen extends Component {
+  state = {
+    stop: {
+      show: true,
+      remove: false,
+    },
+  };
+
+  loaded = {
+    hide: () => {
+      this.setState({ stop: { show: false } });
+    },
+    remove: () => {
+      this.setState({ stop: { remove: true } });
+    },
+    show: () => {
+      this.setState({ stop: { show: true, remove: false } });
+    },
+  };
+
   render() {
     return (
       <Router>
+        {this.state.stop.remove ? "" : <Loader stop={!this.state.stop.show} />}
         <Header></Header>
         <Content>
           <Routes>
-            <Route path="/table" element={<Table />} />
-            <Route path="/table/:atom" element={<Info />} />
+            <Route path="/table" element={<Table loaded={this.loaded} />} />
+            <Route
+              path="/table/:atom"
+              element={<Info loaded={this.loaded} />}
+            />
           </Routes>
         </Content>
         <Footer></Footer>
