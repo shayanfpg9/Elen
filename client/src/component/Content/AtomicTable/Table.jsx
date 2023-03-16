84785cd21c7af8d4eed165307b8c3acbffb4c02a
shayanfpg9 <shayanfpg9@duck.com>
2023-03-17 00:06:01 +0330

    add search option

--- client/src/component/Content/AtomicTable/Table.jsx
+++ client/src/component/Content/AtomicTable/Table.jsx
@@ -8,6 +8,7 @@ import { media } from "../CssComponents/Util";
 import _ from "lodash";
 import { WithMultiContext } from "../../HOC/WithMultiContext";
 import { WithHook } from "../../HOC/WithHooks";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 //row and cols same styles
 const RowCol = styled.span`
@@ -21,17 +22,13 @@ const RowCol = styled.span`
 `;
 
 //rows
-const Row = styled(RowCol).attrs({
-  role: "rowheader",
-})`
+const Row = styled(RowCol)`
   margin: 0.1rem 0.5rem;
   grid-row: ${(props) => +props.row};
 `;
 
 //cols
-const Col = styled(RowCol).attrs({
-  role: "columnheader",
-})`
+const Col = styled(RowCol)`
   margin: 0 0.1rem;
   margin-bottom: 1rem;
   grid-column: ${(props) => +props.col};
@@ -78,25 +75,49 @@ class Table extends Component {
     theme: {
       name: this.props.Theme.theme,
     },
+    location: window?.location.href,
   };
 
   GetDatas = async () => {
     this.props.Loaded.show();
 
-    //get datas with axios
-    let data = (await axios.get("/api/atom")).data;
+    try {
+      let data;
 
-    data = data.map((prop) => {
-      return <Atom key={prop.name} {...prop} />;
-    });
+      if (this.props?.params?.query) {
+        //search all of the atoms with "this.props.params.atom" property
+        data = (
+          await axios.post("/api/atom/search", {
+            q: this.props.params.query,
+          })
+        ).data.results;
+      } else {
+        //get datas with axios
+        data = (await axios.get("/api/atom")).data;
+      }
 
-    this.props.Loaded.hide();
+      if (!Array.isArray(data) || data.length === 1) {
+        if (Array.isArray(data)) data = data[0];
 
-    this.setState({
-      atoms: data,
-      cols: this.state.cols,
-      rows: this.state.rows,
-    });
+        this.props.navigate(`/atom/${data.name}`);
+      } else {
+        data = data?.map((prop) => <Atom key={prop.name} {...prop} />);
+
+        this.props.Loaded.hide();
+
+        this.setState({
+          atoms: data,
+          cols: this.state.cols,
+          rows: this.state.rows,
+        });
+      }
+    } catch (e) {
+      this.props.Loaded.hide();
+
+      this.setState({
+        atoms: null,
+      });
+    }
   };
 
   setTheme = () => {
@@ -149,6 +170,7 @@ class Table extends Component {
 
   async componentDidMount() {
     await this.GetDatas();
+
     this.setTheme();
 
     document.querySelectorAll(".phase__item, .category__item").forEach((el) => {
@@ -178,6 +200,13 @@ class Table extends Component {
       await this.GetDatas();
     }
 
+    if (this.state.location !== window?.location.href) {
+      this.setState({
+        location: window?.location.href,
+      });
+      await this.GetDatas();
+    }
+
     if (this.state.theme.name !== this.props.Theme.theme) {
       this.setTheme();
     }
@@ -201,6 +230,8 @@ class Table extends Component {
     if (!_.isNull(this.state.atoms)) {
       return (
         <ThemeProvider theme={this.state.theme}>
+          {this.props?.params?.query && <h2>جواب جست ‌وجو:</h2>}
+
           <section className="table">
             {/* columns: */}
             {Cols}
@@ -209,13 +240,26 @@ class Table extends Component {
             {Rows}
 
             <FreeSpace />
-            <Categories />
-            <Phase />
+
+            {!this.props?.params?.query && [
+              <Categories key="category" />,
+              <Phase key="phase" />,
+            ]}
 
             {/* import datas */}
             {this.state.atoms}
           </section>
+
+          {this.props?.params?.query && <Link to="/table">برگشت به جدول</Link>}
         </ThemeProvider>
+      );
+    } else {
+      return (
+        <>
+          <h2>عنصر مورد نظر یافت نشد</h2>
+          <h3>سعی کنید انگلیسی جست‌وجو کنید</h3>
+          <Link to="/table">برگشت به جدول</Link>
+        </>
       );
     }
   }
@@ -223,5 +267,15 @@ class Table extends Component {
 
 export default WithHook(
   WithMultiContext(Table, ["Refresh", "Theme", "Loaded"]),
-  "useSearch"
+  [
+    "useSearch",
+    {
+      name: "params",
+      HookFunc: useParams,
+    },
+    {
+      name: "navigate",
+      HookFunc: useNavigate,
+    },
+  ]
 );
