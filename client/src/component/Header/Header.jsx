808c204527c39d0b3d0e299b6bda34006bc8469e
shayanfpg9 <shayanfpg9@duck.com>
2023-03-16 19:31:52 +0330

    import searchbox

--- client/src/component/Header/Header.jsx
+++ client/src/component/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useSize } from "../Hook/hooks.jsx";
 import Menu from "./Menu.jsx";
+import Search from "./Search.jsx";
 import { BsList } from "react-icons/bs";
 
 export default function Header() {
@@ -16,24 +17,47 @@ export default function Header() {
     else setStatus(true);
   };
 
+  const [searchBox, setSearchBox] = useState(false);
+  const SearchBoxClick = () => {
+    if (searchBox) setSearchBox(false);
+    else setSearchBox(true);
+
+    MenuBtnClick();
+  };
+
   return (
     <>
       <span className="header--top"></span>
       <header className="header">
-        <img src="" alt="شیمی کده مجازی الن" className="header__logo fibo-1--ci" />
+        <img
+          src=""
+          alt="شیمی کده مجازی الن"
+          className="header__logo fibo-1--ci"
+        />
         <h1 className="header__title">شیمی کده مجازی الن</h1>
 
         {width >= useSize("xl") ? (
-          <Menu></Menu>
+          <Menu SearchBoxClick={SearchBoxClick}></Menu>
         ) : (
           <i onClick={MenuBtnClick} className="header__menu-icon">
             <BsList />
           </i>
         )}
       </header>
+      {searchBox && (
+        <Search
+          close={() => {
+            setSearchBox(false);
+          }}
+        />
+      )}
 
       {width < useSize("xl") && (
-        <Menu status={status} function={MenuBtnClick} />
+        <Menu
+          status={status && !searchBox}
+          function={MenuBtnClick}
+          SearchBoxClick={SearchBoxClick}
+        />
       )}
     </>
   );
