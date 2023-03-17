e5a276f43b68e01eaeee7edd83928385335edf6e
shayanfpg9 <shayanfpg9@duck.com>
2023-03-17 17:47:47 +0330

    connect to indexedDB

---
+++ client/src/component/funcs/DB.js
@@ -0,0 +1,58 @@
+import { message } from "./funcs";
+
+export default class DB {
+  constructor(dbname) {
+    const OpenReq = indexedDB.open("Elen", 3);
+
+    this.dbname = dbname;
+
+    OpenReq.onupgradeneeded = () => {
+      const db = OpenReq.result;
+
+      db.createObjectStore(dbname, { keyPath: "number" });
+    };
+
+    OpenReq.onsuccess = () => {
+      const db = OpenReq.result;
+
+      db.onversionchange = function () {
+        db.close();
+        message("خطا", "ما صفحه را رفرش می کنیم...", "error").then(() => {
+          window?.location.reload();
+        });
+      };
+    };
+  }
+
+  get(callback) {
+    const OpenReq = indexedDB.open("Elen", 3);
+
+    OpenReq.onsuccess = () => {
+      const db = OpenReq.result;
+      const transaction = db.transaction(this.dbname, "readwrite");
+
+      const Atoms = transaction.objectStore(this.dbname);
+
+      const all = Atoms.getAll();
+
+      all.onsuccess = () => {
+        callback(all.result);
+      };
+    };
+  }
+
+  set(items = [{}]) {
+    const OpenReq = indexedDB.open("Elen", 3);
+
+    OpenReq.onsuccess = () => {
+      const db = OpenReq.result;
+      const transaction = db.transaction(this.dbname, "readwrite");
+
+      const Atoms = transaction.objectStore(this.dbname);
+
+      Atoms.clear();
+
+      items.forEach((obj) => Atoms.add(obj));
+    };
+  }
+}
