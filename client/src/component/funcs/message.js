57d07d8adf8cb6e09ef4db8d140808b9ad7241a5
shayanfpg9 <shayanfpg9@duck.com>
2023-03-17 17:46:40 +0330

    make message util func

---
+++ client/src/component/funcs/message.js
@@ -0,0 +1,19 @@
+import Swal from "sweetalert2";
+
+const Toast = Swal.mixin({
+  background: "var(--color-fg)",
+  color: "var(--color-text)",
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
+
+export default function message(title = "", text = "", icon = "success") {
+  return Toast.fire({ ...{ title, icon }, html: text });
+}
