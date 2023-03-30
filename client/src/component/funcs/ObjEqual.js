e1546cb2b59914ab7a3f13d7e2881a47ba996468
shayanfpg9 <shayanfpg9@duck.com>
2023-03-30 13:32:50 +0430

    delete equal func

--- client/src/component/funcs/ObjEqual.js
+++
@@ -1,7 +0,0 @@
-export default function equal(obj1, obj2) {
-  obj1 = JSON.stringify(obj1);
-  obj2 = JSON.stringify(obj2);
-
-  //when you convert anything to JSON you get a string...
-  return obj1 === obj2;
-}
--- client/src/component/funcs/funcs.js
+++ client/src/component/funcs/funcs.js
@@ -1,7 +1,6 @@
 import getSize from "./getSize";
-import equal from "./ObjEqual";
 import DB from "./DB";
 import message from "./message";
 import writeEffect from "./writeEffect";
 
-export { getSize, equal, DB, message, writeEffect };
+export { getSize, DB, message, writeEffect };
